498023b3da2a74c4275abf0357a8f07591a3fa92
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-08-20 03:40:21 -0500

    Refactor to deal with dynamic or unknown JSON structures
    Now the data miner output 3 files:
    
    - list.json: lists all the products for each drugstore
    - products.json: lists all the products
    - drugstores.json: lists all the drugstores

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module observatorium-data-miner
 go 1.13
 
 require (
-	github.com/hashicorp/go-retryablehttp v0.6.7 // indirect
-	github.com/schollz/progressbar/v3 v3.3.4 // indirect
+	github.com/Jeffail/gabs/v2 v2.6.0
+	github.com/hashicorp/go-retryablehttp v0.6.7
+	github.com/schollz/progressbar/v3 v3.3.4
 )
--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
+	"github.com/Jeffail/gabs/v2"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/schollz/progressbar/v3"
 	"io/ioutil"
-	"regexp"
 	"strconv"
 	"strings"
 	"sync/atomic"
@@ -17,237 +16,221 @@ const ProductsUrl = "https://opmcovid.minsa.gob.pe/observatorio/precios.aspx/Get
 const PricesUrl = "https://opmcovid.minsa.gob.pe/observatorio/wsObservatorio.asmx/listPrice"
 const PharmaUrl = "https://opmcovid.minsa.gob.pe/observatorio/wsObservatorio.asmx/loadDataPharma"
 
-type productPrice struct {
-	Product   Product   `json:"Product"`
-	DrugStore DrugStore `json:"drugstore"`
+type Result struct {
+	ubigeoId   int
+	products   map[int]*gabs.Container
+	drugstores map[string]*gabs.Container
+	resultList map[string]*gabs.Container
 }
 
-type ubigeo struct {
-	Id   int            `json:"id_ubigeo"`
-	Data []productPrice `json:"data"`
-}
-
-type DrugStore struct {
-	Ruc       string `json:"ruc"`
-	Name      string `json:"nombre"`
-	Address   string `json:"direccion"`
-	Location  string `json:"ubicacion"`
-	Type      string `json:"tipo"`
-	Phone     string `json:"telefono"`
-	OpenHours string `json:"horario"`
-}
-
-type Product struct {
-	DrugStoreId    string  `json:"codigo"`
-	GenericName    string  `json:"nombre"`
-	MarketName     string  `json:"b"`
-	Concentration  string  `json:"c"`
-	Price          float64 `json:"precio"`
-	Form           string  `json:"d"`
-	Presentations  string  `json:"f"`
-	Laboratory     string  `json:"laboratorio"`
-	Manufacturer   string  `json:"k"`
-	SearchName     string  `json:"l"`
-	UpdatedAt      string  `json:"fecha"`
-	Sector         string  `json:"setcodigo"`
-	ProductId      int     `json:"codprod"`
-	HealthRegistry string  `json:"regsan"`
-}
-
-// var wg sync.WaitGroup
 var client = retryablehttp.NewClient()
 
 func main() {
 	client.Logger = nil
 	client.RetryMax = 10
-	ubigeos := getAllUbigeos()
+
+	ubigeos := readUbigeos()
 	names := getProductsName()
-	productsUbigeo := make(chan ubigeo, len(ubigeos))
+
+	listByUbigeo := make(chan Result, len(ubigeos))
+
 	bar := progressbar.Default(int64(len(ubigeos) * len(names)))
 	var done uint64
 
-	for _, ubigeo := range ubigeos {
-		go generatePriceListByUbigeo(strconv.Itoa(ubigeo.Id), names, productsUbigeo, &done)
+	for _, ubigeoId := range ubigeos {
+		go generateListByUbigeo(ubigeoId, names, listByUbigeo, &done)
 	}
 
 	for {
 		_ = bar.Set(int(done))
-		if len(productsUbigeo) == cap(productsUbigeo) {
+		if len(listByUbigeo) == cap(listByUbigeo) {
 			// Channels are full
 			break
 		}
 	}
 
-	close(productsUbigeo)
+	close(listByUbigeo)
 
-	var result []ubigeo
+	resultObj := gabs.New()
+	productsObj := gabs.New()
+	drugstoresObj := gabs.New()
 
-	for u := range productsUbigeo {
-		result = append(result, u)
+	for u := range listByUbigeo {
+
+		_, err := resultObj.Set(u.resultList, strconv.Itoa(u.ubigeoId))
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = productsObj.Set(u.products)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = drugstoresObj.Set(u.drugstores)
+		if err != nil {
+			panic(err)
+		}
+
 	}
 
-	dataBytes, err := json.Marshal(result)
-	if err != nil {
-		panic(err)
-	}
-
-	err = ioutil.WriteFile("one_big_fucking_product.json", dataBytes, 0644)
-	if err != nil {
-		panic(err)
-	}
-
+	writeFile("products.json", productsObj.Bytes())
+	writeFile("drugstores.json", drugstoresObj.Bytes())
+	writeFile("list.json", resultObj.Bytes())
 }
 
-func generatePriceListByUbigeo(ubigeoId string, productsNames []string, c chan ubigeo, done *uint64) {
-	var drugStoreMap = make(map[string]DrugStore)
-	var productsMap = make(map[int]Product)
-	ubiID, _ := strconv.Atoi(ubigeoId)
-	result := ubigeo{Id: ubiID}
-	for _, name := range productsNames {
-		products := getProductPrices(name, ubigeoId, 1)
-		for _, p := range products {
-			drugstore, product := fillProductData(p, productsMap, drugStoreMap)
-			finalList := productPrice{DrugStore: drugstore, Product: product}
-			result.Data = append(result.Data, finalList)
-			atomic.AddUint64(done, 1)
-		}
+func writeFile(name string, content []byte) {
+	err := ioutil.WriteFile(name, content, 0644)
+	if err != nil {
+		panic(err)
 	}
+}
+
+func generateListByUbigeo(ubigeoId int, productsNames []string, c chan Result, done *uint64) {
+	var drugStoreMap = make(map[string]*gabs.Container)
+	var productsMap = make(map[int]*gabs.Container)
+	var list = make(map[string]*gabs.Container)
+
+	for _, name := range productsNames {
+		products := getList(name, ubigeoId, 1)
+		for _, item := range products {
+			productCode := int(item.S("codprod").Data().(float64))
+			product, hasProduct := productsMap[productCode]
+
+			drugstoreCode := item.S("codigo").Data().(string)
+			drugstore, hasDrugstore := drugStoreMap[drugstoreCode]
+
+			if !(hasProduct && hasDrugstore) {
+				product, drugstore = getDrugstore(drugstoreCode, productCode)
+				productsMap[productCode] = product
+				drugStoreMap[drugstoreCode] = drugstore
+			}
+
+			combinedKey := fmt.Sprintf("%d-%s", productCode, drugstoreCode)
+			list[combinedKey] = item
+		}
+		atomic.AddUint64(done, 1)
+	}
+
+	result := Result{ubigeoId: ubigeoId, products: productsMap, drugstores: drugStoreMap, resultList: list}
+
 	c <- result
 }
 
-func fetchWrapper(url, body string) []string {
-	search := []byte(body)
-
-	r, err := client.HTTPClient.Post(url, "application/json", bytes.NewBuffer(search))
-	if err != nil {
-		panic(err)
-	}
-	defer r.Body.Close()
-
-	obj := make(map[string][]string)
-	err = json.NewDecoder(r.Body).Decode(&obj)
-
-	return obj["d"]
-}
-
-func getProductPrices(name, ubigeo string, fromPage int) []Product {
+func getList(name string, ubigeo int, fromPage int) []*gabs.Container {
 	search := fmt.Sprintf(`
-{
-	"typeup": "FARMACIA",
-	"tipo": "M",
-	"nomprod": "%s",
-	"ubigeo": "%s",
-	"type": "0",
-	"labotarorio": "",
-	"establecimiento": "",
-	"pag": %d
-}
-`, name, ubigeo, fromPage)
-
-	resp := fetchWrapper(PricesUrl, search)
-
-	type productList struct {
-		Data []Product `json:"products"`
+	{
+		"typeup": "FARMACIA",
+		"tipo": "M",
+		"nomprod": "%s",
+		"ubigeo": "%s",
+		"type": "0",
+		"labotarorio": "",
+		"establecimiento": "",
+		"pag": %d
 	}
+	`, name, strconv.Itoa(ubigeo), fromPage)
 
-	var products productList
+	resp := fetchWrapper(PricesUrl, search).Children()
 
-	data := []byte(fmt.Sprintf(`{"products": %s}`, resp[0]))
-	if err := json.Unmarshal(data, &products); err != nil {
+	list, err := gabs.ParseJSON([]byte(resp[0].Data().(string)))
+	if err != nil {
 		panic(err)
 	}
 
-	re := regexp.MustCompile(`\d+`)
-	currentPage, _ := strconv.Atoi(string(re.Find([]byte(resp[1]))))
+	result := list.Children()
 
-	if fromPage < currentPage {
-		products.Data = append(products.Data, getProductPrices(name, ubigeo, fromPage+1)...)
+	pages, err := gabs.ParseJSON([]byte(resp[1].Data().(string)))
+	if err != nil {
+		panic(err)
 	}
 
-	return products.Data
+	lastPage := int(pages.Children()[0].S("tpaginas").Data().(float64))
+
+	if fromPage < lastPage {
+		result = append(result, getList(name, ubigeo, fromPage+1)...)
+	}
+
+	return result
 }
 
-func fillProductData(firstHalf Product, productMap map[int]Product, drugStoreMap map[string]DrugStore) (DrugStore, Product) {
-	drugStore, hasDrugStore := drugStoreMap[firstHalf.DrugStoreId]
-	product, hasProduct := productMap[firstHalf.ProductId]
-
-	if hasDrugStore && hasProduct {
-		return drugStore, product
-	}
-
-	drugStore, secondHalf := getDrugStore(firstHalf.DrugStoreId, firstHalf.ProductId)
-	drugStoreMap[firstHalf.DrugStoreId] = drugStore
-
-	if secondHalf != (Product{}) {
-		firstHalf.MarketName = secondHalf.MarketName
-		firstHalf.Concentration = secondHalf.Concentration
-		firstHalf.Form = secondHalf.Form
-		firstHalf.Presentations = secondHalf.Presentations
-		firstHalf.Manufacturer = secondHalf.Manufacturer
-		firstHalf.SearchName = secondHalf.SearchName
-
-		productMap[firstHalf.ProductId] = firstHalf
-	}
-
-	return drugStore, firstHalf
-}
-
-func getDrugStore(drugStoreId string, productId int) (DrugStore, Product) {
-
+func getDrugstore(drugStoreId string, productId int) (drugstore, product *gabs.Container) {
 	search := fmt.Sprintf(`{"cod_estab":"%s","cod_prod":%d}`, drugStoreId, productId)
-	resp := fetchWrapper(PharmaUrl, search)
-	drugStoreData := resp[0][1 : len(resp[0])-1]
-	productData := fmt.Sprintf(`{"products": %s}`, resp[1])
+	resp := fetchWrapper(PharmaUrl, search).Children()
 
-	type productWrapper struct {
-		Products []Product `json:"products"`
-	}
-
-	var drugStore DrugStore
-	err := json.Unmarshal([]byte(drugStoreData), &drugStore)
+	rawDrugstore, err := gabs.ParseJSON([]byte(resp[0].Data().(string)))
 	if err != nil {
 		panic(err)
 	}
+	drugstore = rawDrugstore.Children()[0]
 
-	var pw productWrapper
-	err = json.Unmarshal([]byte(productData), &pw)
+	rawProduct, err := gabs.ParseJSON([]byte(resp[1].Data().(string)))
 	if err != nil {
 		panic(err)
 	}
+	product = rawProduct.Children()[0]
 
-	if len(pw.Products) == 0 {
-		pw.Products = append(pw.Products, Product{})
-	}
-
-	return drugStore, pw.Products[0]
+	return
 }
 
 func getProductsName() []string {
 	search := "{'prefix': ''}"
 	raw := fetchWrapper(ProductsUrl, search)
-	var names []string
+	namesSet := make(map[string]bool)
 
-	for _, fullName := range raw {
-
-		short := strings.ReplaceAll(strings.Split(fullName, "-")[0], "\"", "")
+	for _, fullName := range raw.Children() {
+		short := strings.ReplaceAll(strings.Split(fullName.Data().(string), "-")[0], "\"", "")
 		if len(short) < 5 {
 			continue
 		}
 
-		names = append(names, short)
+		_, ok := namesSet[short]
+		if !ok {
+			namesSet[short] = true
+		}
+	}
+
+	var names []string
+
+	for name := range namesSet {
+		names = append(names, name)
 	}
 
 	return names
 }
 
-func getAllUbigeos() []ubigeo {
+func readUbigeos() []int {
 	file, _ := ioutil.ReadFile("ubigeos.json")
 
-	var ubigeos []ubigeo
-	err := json.Unmarshal(file, &ubigeos)
+	ubigeosJson, err := gabs.ParseJSON(file)
 	if err != nil {
 		panic(err)
 	}
 
+	var ubigeos []int
+
+	for _, ubigeo := range ubigeosJson.Children() {
+		ubigeos = append(ubigeos, int(ubigeo.S("id_ubigeo").Data().(float64)))
+	}
+
 	return ubigeos
+}
+
+func fetchWrapper(url, search string) *gabs.Container {
+	resp, err := client.HTTPClient.Post(url, "application/json", bytes.NewBuffer([]byte(search)))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	raw, err := gabs.ParseJSON(body)
+	if err != nil {
+		panic(err)
+	}
+
+	return raw.Path("d")
 }
